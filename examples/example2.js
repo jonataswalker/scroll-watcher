228a855cfee19bb2ed76cfb819651001fc94286e
Jonatas Walker <jonataswalker@gmail.com>
2016-12-12 16:23:45 -0200

    Update examples

--- examples/example2.js
+++ examples/example2.js
@@ -1,18 +1,4 @@
-smoothScroll.init({
-  selectorHeader: '[data-scroll-header]'
-});
-
 var watcher = new ScrollWatcher();
-
-watcher.on('page:load', function (evt) {
-  console.log('page:load');
-  window.setTimeout(() => window.scrollBy(0, 1), 20);
-});
-
-watcher.on('scrolling', function (evt) {
-  console.info(evt);
-});
-
 var watching_els = document.querySelectorAll('#about, #portfolio, #contact');
 var menu = {
   about: document.getElementById('li-about'),
@@ -21,31 +7,42 @@ var menu = {
 };
 var lastActive = menu.about;
 
+smoothScroll.init({
+  selectorHeader: '[data-scroll-header]'
+});
+
+watcher.on('scrolling', function(evt) {
+  // console.log('scrolling', evt);
+});
+
+watcher.on('page:load', function (evt) {
+  window.setTimeout(() => {
+    if (watcher.windowAtBottom()) window.scrollBy(0, -1);
+    else window.scrollBy(0, 1);
+  }, 30);
+});
+
 [].forEach.call(watching_els, function (each) {
   watcher.watch(each, { top: 50, bottom: 0 })
     .on('enter', function (evt) {
-      console.info('entered', evt.target.id, evt);
-
+      // console.info('enter', evt.target.id);
       if (evt.scrollingDown) {
         lastActive.classList.remove('active');
         menu[evt.target.id].classList.add('active');
-
         lastActive = menu[evt.target.id];
       }
     })
     .on('exit', function (evt) {
-      console.info('exited', evt.target.id);
+      // console.info('exited', evt.target.id);
     })
     .on('exit:partial', function (evt) {
-      console.info('partial exited', evt.target.id);
+      // console.info('partial exited', evt.target.id);
     })
     .on('enter:full', function (evt) {
-      console.info('full entered', evt.target.id);
-
+      // console.info('full entered', evt.target.id);
       if (evt.scrollingUp) {
         lastActive.classList.remove('active');
         menu[evt.target.id].classList.add('active');
-
         lastActive = menu[evt.target.id];
       }
     });
