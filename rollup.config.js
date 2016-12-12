4978274b95db18f7cb362b8dd11500415174ac15
Jonatas Walker <jonataswalker@gmail.com>
2016-12-12 14:51:27 -0200

    Change build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,73 @@
-import json from 'rollup-plugin-json';
-import buble from 'rollup-plugin-buble';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+var fs = require('fs'),
+    buble = require('rollup-plugin-buble'),
+    resolve = require('rollup-plugin-node-resolve'),
+    commonjs = require('rollup-plugin-commonjs'),
+    pkg = require('./package.json');
 
-var pkg = require('./package.json');
+var banner = fs.readFileSync('banner.js', 'utf-8')
+  .replace('${name}', pkg.name)
+  .replace('${description}', pkg.description)
+  .replace('${homepage}', pkg.homepage)
+  .replace('${version}', pkg.version)
+  .replace('${time}', new Date());
 
 export default {
   format: 'umd',
-  entry: pkg.rollup.entry,
-  dest: pkg.rollup.dest,
-  moduleName: pkg.rollup.moduleName,
+  entry: pkg.build.entry,
+  dest: pkg.build.dest,
+  moduleName: pkg.build.moduleName,
+  banner: banner,
   plugins: [
-    json({
-      exclude: [ 'node_modules/**' ]
-    }),
-    buble({
-      exclude: ['node_modules/**', '*.json']
-    }),
-    commonjs({
-      namedExports: {
-        // left-hand side can be an absolute path, a path
-        // relative to the current directory, or the name
-        // of a module in node_modules
-        'tiny-emitter': [ 'TinyEmitter' ]
-      }
-    }),
-    nodeResolve()
+    resolve({ browser: true }),
+    commonjs({ namedExports: { 'tiny-emitter': ['TinyEmitter'] }}),
+    buble()
   ]
-}
+};
+
+/*  entry: pkg.build.entry,
+  plugins: [
+  ]
+}).then(bundle => {
+  const resultBundle = bundle.generate({
+    format: 'umd',
+    moduleName: pkg.build.moduleName,
+    dest: pkg.build.dest,
+  });
+
+  const minified = minify(resultBundle.code, {
+    fromString: true,
+    outSourceMap: pkg.build.destMinMap.replace('build/', ''),
+    warnings: true,
+    mangle: true,
+    output: { comments: /^!/ },
+    compress: {
+      screw_ie8: true,
+      drop_console: true
+    }
+  });
+
+  fs.writeFileSync(pkg.build.dest, resultBundle.code);
+  fs.writeFile(pkg.build.destMin, minified.code, (err) => {
+    if (err) throw err;
+
+    const bundleSize = bytes(Buffer.byteLength(resultBundle.code));
+    const minSize = bytes(Buffer.byteLength(minified.code));
+    const bundleGzip = bytes(gzip.sync(resultBundle.code));
+    const minGzip = bytes(gzip.sync(minified.code));
+
+    console.log(boxen(
+      chalk.green.bold('Bundle size: ') +
+      chalk.yellow.bold(bundleSize) + ', ' +
+      chalk.green.bold('Gzipped size: ') +
+      chalk.yellow.bold(bundleGzip), { padding: 1 }
+    ));
+    console.log(boxen(
+      chalk.green.bold('Minified size: ') +
+      chalk.yellow.bold(minSize) + ', ' +
+      chalk.green.bold('Gzipped size: ') +
+      chalk.yellow.bold(minGzip), { padding: 1 }
+    ));
+
+    fs.writeFileSync(pkg.build.destMinMap, minified.map);
+  });
+});*/
